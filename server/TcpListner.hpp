3cfea740b0a4eac65ded325161e72b04839ff2e7
wirena <wirena@protonmail.com>
2022-07-29 09:43:47 +0300

    redesign tcp listner

--- server/TcpListner.hpp
+++ server/TcpListner.hpp
@@ -2,24 +2,71 @@
 #define NTECHLAB_TESTTASK_TCPLISTNER_HPP
 #include <boost/asio.hpp>
 #include <iostream>
+#include "fail.h"
+#include <boost/asio/strand.hpp>
+#include <boost/beast.hpp>
+#include <iostream>
+
 
 template<class Connection>
 class TcpListner {
+    const Connection::MuxFunction muxFunction;
     using tcp = boost::asio::ip::tcp;
     using error_code = boost::system::error_code;
-    using ConnectionType = std::shared_ptr<Connection>;
-
     boost::asio::io_context &ioc;
     tcp::acceptor acceptor;
 
     void accept();
     void onAccept(error_code ec, tcp::socket socket);
 public:
-    explicit TcpListner(boost::asio::io_context &ioc) : acceptor(ioc), ioc(ioc) {}
+    explicit TcpListner(boost::asio::io_context &ioc, const Connection::MuxFunction&muxFunction) : acceptor(ioc), ioc(ioc),muxFunction(muxFunction)  {}
 
     error_code listen(const tcp::endpoint &endpoint);
     ~TcpListner() { std::cout << "Exiting" << std::endl; }
 };
+
+
+template<class Connection>
+boost::system::error_code TcpListner<Connection>::listen(const tcp::endpoint &endpoint) {
+    error_code err;
+    acceptor.open(endpoint.protocol(), err);
+    if (err) {
+        fail(err, "Acceptor.open failed");
+        return err;
+    }
+
+    acceptor.bind(endpoint, err);
+    if (err) {
+        fail(err, "Acceptor.bind failed");
+        return err;
+    }
+    acceptor.listen(boost::asio::socket_base::max_listen_connections, err);
+    if (err) {
+        fail(err, "Acceptor.listen failed");
+        return err;
+    }
+    accept();
+    return error_code();
+}
+
+template<class Connection>
+void TcpListner<Connection>::onAccept(error_code err, tcp::socket socket) {
+    std::cout << "onAccept" << std::endl;
+    if (err) {
+        fail(err, "Accept failed");
+        return;
+    } else {
+        std::make_shared<Connection>(std::move(socket))->connect();
+    }
+    accept();
+}
+
+template<class Connection>
+void TcpListner<Connection>::accept() {
+    acceptor.async_accept(boost::asio::make_strand(ioc), boost::beast::bind_front_handler(
+            &TcpListner::onAccept, this));
+    std::cout << "Accept" << std::endl;
+}
 
 
 #endif//NTECHLAB_TESTTASK_TCPLISTNER_HPP
--- server/main.cpp
+++ server/main.cpp
@@ -1,13 +1,21 @@
-#include "TcpListner.h"
+#include "TcpListner.hpp"
+#include "HTTPConnection.h"
 #include <boost/asio.hpp>
 #include <boost/asio/signal_set.hpp>
 #include <boost/bind.hpp>
 #include <iostream>
+
+
 using tcp = boost::asio::ip::tcp;
 using error_code = boost::system::error_code;
 
 
 void boost::throw_exception(std::exception const &e) {
+    std::cerr << "aborting" << std::endl;
+    abort();
+}
+
+void boost::throw_exception(std::exception const &, boost::source_location const &) {
     std::cerr << "aborting" << std::endl;
     abort();
 }
@@ -18,7 +26,7 @@ int main(int argc, char *argv[]) {
     boost::asio::io_context ioc;
     boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
     signals.async_wait(boost::bind(&boost::asio::io_service::stop, &ioc));
-    TcpListner listner(ioc);
+    TcpListner<HTTPConnection> listner(ioc);
     listner.listen(tcp::endpoint(boost::asio::ip::make_address("127.0.0.1", err), 8080));
     ioc.run();
 }

1843ffcde998d7e447d1d30c5b4df0a26dd822dc
wirena <wirena@protonmail.com>
2022-07-30 22:31:30 +0300

    Command line options parsing

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,10 @@
 #include "HTTPServer.h"
+#include <cstdlib>
+#include <getopt.h>
 #include <iostream>
+#include <limits>
+
+void printHelp(){};
 
 using tcp = boost::asio::ip::tcp;
 using error_code = boost::system::error_code;
@@ -25,14 +30,50 @@ const auto handler = [](boost::beast::http::message<true, boost::beast::http::st
     callback(std::move(res));
 };
 
+bool parseArguments(int argc, char *argv[], int *threadsNumber, std::string *ip, std::string *port) {
+    int c;
+    char *endPtr;
+    long argNumber;
+    while ((c = getopt(argc, argv, "hi:p:j:")) != -1)
+        switch (c) {
+            case 'i':
+                *ip = optarg;
+                break;
+            case 'p':
+                *port = optarg;
+                break;
+            case 'j':
+                argNumber = std::strtol(optarg, &endPtr, 10);
+                if (endPtr == optarg || *endPtr != '\0' ||
+                    argNumber > std::numeric_limits<int>::max() || argNumber <= 0) {
+                    std::cerr << "Invalid argument for -j option" << std::endl;
+                    return false;
+                }
+                *threadsNumber = static_cast<int>(argNumber);
+                break;
+            case '?':
+                std::cerr << "Invalid option " << static_cast<char>(optopt) << std::endl;
+                return false;
+            default:
+                std::cerr << "Invalid option " << c << std::endl;
+                return false;
+        }
+    return true;
+}
+
+
 int main(int argc, char *argv[]) {
+    std::string ipAddress{"127.0.0.1"};
+    std::string port{"8080"};
+    int threadsNumber = 1;
+    if (!parseArguments(argc, argv, &threadsNumber, &ipAddress, &port))
+        return 1;
+    std::cout << "Starting server on " << ipAddress << ':' << port << " using " << threadsNumber << " threads" << std::endl;
     HTTPServer httpServer;
-    httpServer.bindTo("127.0.0.1", "8080");
-    //httpServer.stopBlockingOnSignals({SIGINT, SIGTERM});
-    httpServer.setThreadNumber(1);
+    httpServer.bindTo(ipAddress, port);
+    httpServer.stopBlockingOnSignals({SIGINT, SIGTERM});
+    httpServer.setThreadNumber(threadsNumber);
     httpServer.setHandler({"POST", "/"}, handler);
-    //httpServer.runBlocking();
-    httpServer.runNonBlocking();
-    sleep(10);
-    httpServer.stopNonBlocking();
+    httpServer.runBlocking();
+    return 0;
 }
